733fcd10dee9fb82a0e1f1a353a97453e3a9e22b
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 21:53:16 +0200

    Add project files.

---
+++ Ex_w_2_l_4_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using static System.Math;
+
+namespace Ex_w_2_l_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, d, k;
+            Console.WriteLine("Podaj pierwszy bok");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugi bok");
+            b = double.Parse(Console.ReadLine());
+
+            k = Pow(a,2) + Pow(b,2);    //d - kwadrat przekątnej
+
+            d = Math.Sqrt(k);
+
+            Console.WriteLine("Przekątna ma długość:  " + d);
+
+        }
+    }
+}
